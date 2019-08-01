a6c0eaba835d36e7efab2466bfdf1b03c5de1cf6
Mohammed Abdulkereem <mohammed-io@outlook.com>
2019-08-01 21:53:14 +0300

    explaining

--- src/index.js
+++ src/index.js
@@ -1,12 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from "react-dom";
+import React from "react";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function FirstComponent(props) {
+  const [s, setS] = React.useState(0);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  React.useEffect(() => {
+    setInterval(() => {
+      setS(s + 1);
+    }, 500);
+  }, [1]);
+
+  return (
+    <>
+      <h1 className="red">
+        <i>Hello</i>
+        {props.name}I am {props.age}
+      </h1>
+      <h1>HEllo {s}</h1>
+    </>
+  );
+}
+
+class SecondComponent extends React.Component {
+  state = {
+    count: 0
+  };
+
+  increment = e => {
+    e.persist(); // for the async
+    setTimeout(() => {
+      console.log(e.target);
+    }, 1000);
+    this.setState(state => ({ count: state.count + 1 })); // function for handling the async rendering situation
+    this.setState(state => ({ count: state.count + 1 }));
+    this.setState(state => ({ count: state.count + 1 }));
+  };
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <>
+        <h1 className="red">
+          <i>Hello</i>
+          {this.props.name}I am {this.props.age}
+        </h1>
+        <h1>HEllo {this.state.count}</h1>
+
+        <button onClick={this.increment}>Inc</button>
+      </>
+    );
+  }
+}
+
+ReactDOM.render(
+  // FirstComponent({ name: "Wisam", age: 20 }),
+  // <FirstComponent name="Wisam" age={20} />,
+  <SecondComponent name="Wisam" age={20} />,
+  document.getElementById("root")
+);
+
+const initState = 0;
+const createStore = (reducer, state = initState) => {
+  const getState = () => state;
+
+  const listeners = [];
+  const subscribe = f => {
+    listeners.push(f);
+  };
+};
